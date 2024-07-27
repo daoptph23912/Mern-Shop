4ac50e837a2aa5e4144524f5ae624f256a77104c
daoptph23912 <daoptph23912@fpt.edu.vn>
2024-07-27 09:39:15 +0700

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.set('strictQuery', true);
-    const conn = await mongoose.connect(process.env.MONGO_URL,{
+    mongoose.set("strictQuery", true);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
-import express from "express"
-import dotenv from "dotenv"
-import colors from "colors"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
-import connectDB from "./config/db.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-dotenv.config()
-connectDB()
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+connectDB();
 
-const app = express()
-app.use(express.json())
-app.get('/', (req,res) => {
-  res.send('API is running.....')
-})
-app.use('/api/products', productRoutes) //fetch data from mongo
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running.....");
+});
+app.use("/api/products", productRoutes); //fetch data from mongo
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
